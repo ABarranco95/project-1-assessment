33b4ce3e4ebb6612c5e4c798c1a439dbfeed3adc
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-11-12 07:33:54 -0800

    [main.js] Subtracting function is working. Adding function just adds number to the end of current value.

--- main.js
+++ main.js
@@ -2,19 +2,23 @@
 
 let add = document.querySelector('.add');
 let subtract = document.querySelector('.subtract');
+let userInput = document.querySelector('.user-input');
 let currentValue = document.querySelector('p');
 
+console.log(userInput.value);
 // Start our count at 0 upon loading and display it
 count = 0 
 currentValue.innerHTML = count
 
+
+
 // Add and subtract functions
 add.onclick = function() {
-    count += 1;
+    count + userInput.value;
     currentValue.innerHTML = count;
 }
 
 subtract.onclick = function() {
-    count -= 1;
+    count -= userInput.value;
     currentValue.innerHTML = count;
 }
